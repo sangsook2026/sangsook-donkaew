7a148de1eee6c0804d612657fe1946636d010b57
sangsook2026 <sangsook1268@gmail.com>
2026-08-19 15:03:40 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,10 @@
 window.SANGSOOK_CONFIG = {
   // วาง URL เว็บแอป Google Apps Script ตัวจริงที่ลงท้ายด้วย /exec
-  appUrl: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_EXEC_URL_HERE",
+  appUrl: "https://script.google.com/macros/s/AKfycbxYZNy_pJzM8Q6rQ0GzU53ELH5nUyrDfExcqyzxqi7NE1iMZdPn446kKjvKVOy5Ce9IkQ/exec",
 
   // ถ้ามี Facebook Page ให้ใส่ URL เช่น https://www.facebook.com/yourpage
   // ระบบจะสร้างปุ่มไปยังเพจให้โดยอัตโนมัติ
-  facebookPageUrl: "",
+  facebookPageUrl: "https://www.facebook.com/dk.hospital?locale=th_TH",
 
   siteUrl: "https://sangsook2026.github.io/sangsook-donkaew/"
 };
